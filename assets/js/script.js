c68d06cc158440f852ace0103464aa5107eb554c
Neer Bdr. Shrestha <neer.bahadur.shrestha@md.p2h.com>
2024-08-08 16:56:18 +0545

    feat: added pricing and faq section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,48 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("Hello World!");
+  navTab();
+
+  const accordions = document?.querySelectorAll(".accordion");
+  if (accordions) {
+    accordions.forEach((accordion) => {
+      new Accordion(accordion, {
+        duration: 400,
+        openOnInit: [0],
+      });
+    });
+  }
 });
+
+function navTab() {
+  const activeClass = "btn--primary";
+  const tabWrapper = document.querySelectorAll("[data-tab]");
+  if (tabWrapper) {
+    tabWrapper.forEach((tab) => {
+      const tabButtons = tab?.querySelectorAll("[data-tab-btn]");
+      const tabContents = tab?.querySelectorAll("[data-tab-content]");
+
+      tabButtons.forEach((btn) => {
+        btn.addEventListener("click", (event) => {
+          const target = btn.getAttribute("data-target");
+          const targetContent = document.querySelector(target);
+
+          collapseAllTab();
+          makeActiveTab(btn, targetContent);
+        });
+      });
+
+      const collapseAllTab = () => {
+        tabButtons.forEach((btn) => {
+          btn.classList.remove(activeClass);
+        });
+        tabContents.forEach((content) => {
+          content.setAttribute("aria-expanded", "false");
+        });
+      };
+
+      const makeActiveTab = (btn, content) => {
+        btn.classList.add(activeClass);
+        content.setAttribute("aria-expanded", "true");
+      };
+    });
+  }
+}
